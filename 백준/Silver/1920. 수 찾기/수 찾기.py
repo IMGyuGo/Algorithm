d42c80f5c1d0a98eec36ab54854319aeaa473449
IMGyuGo <gyugo4894@gmail.com>
2026-03-14 11:53:13 +0900

    [Silver IV] Title: 수 찾기, Time: 484 ms, Memory: 48616 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,29 @@
+n = int(input())
+qList = list(map(int, input().split()))
+m = int(input())
+aList = list(map(int, input().split()))
+
+qList = sorted(qList)
+
+def search(arr, s, e, target) :
+  # 중앙값
+  mid = (s + e) // 2
+  
+  # base condition 생각
+  if e <= s :
+    if arr[mid] == target :
+      return 1
+    else :
+      return 0
+  
+  if arr[mid] > target :
+    return search(arr, s, mid-1, target)
+  elif arr[mid] < target :
+    return search(arr, mid+1, e, target)
+  else :
+    return 1
+  
+
+for i in range(m) :
+  target = aList[i]
+  print(search(qList, 0, n-1, target))
